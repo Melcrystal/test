7e3d704e57c58a8b28a9a61bacdcc138761ae2b5
Landeau <guilhem.landeau@capgemini.com>
2017-10-10 10:53:26 +0200

    1st commit

---
+++ test/src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		String esp = " ";
+		if (esp.equals(" ")){
+			System.out.println("null"); ;
+		}else System.out.println("not null");
+
+	}
+
+}
