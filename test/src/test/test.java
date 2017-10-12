db3a9bc7fd9a291d8de86380a64d0a676931ce1c
Landeau <guilhem.landeau@capgemini.com>
2017-10-12 15:25:38 +0200

    df

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,12 +1,7 @@
 package test;
 
 public class test {
-<<<<<<< HEAD
 	// ceci est une modification sur la dev
-=======
-	// modif us spa 125
->>>>>>> branch 'US_spa_125' of https://github.com/Melcrystal/test.git
-
 	public static void main(String[] args) {
 		String esp = " ";
 		if (esp.equals(" ")){
